c329e3ccd1dbdb791949434c4db56f56d1e5f9da
alexandregama <alexandre.gama.lima@gmail.com>
2015-10-27 22:11:12 -0200

    Adiciona o Twitter Bootstrap e melhora o layout da tabela de Produtos

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
   get "produtos" => "produtos#index" # get [nome do link que queremos] => [nome do controller]#[nome da pagina em views]
-
+  root "produtos#index"
 end
